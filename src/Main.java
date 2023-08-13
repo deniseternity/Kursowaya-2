a627e3d6ab2b4567d3fa65025e8f011a19f42b76
Magay Denis <qwertydenis009@gmail.com>
2023-08-13 20:33:43 +0300

    курсовая (5 билет).

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] massiv = {1, 2, 4, 5, 7, 45, 34};
-        int[] backMassiv = {4, 5, 7, 45, 34}
+        int[] backMassiv = {4, 5, 7, 45, 34};
         for (int i = 0; i < massiv.length; i++) {
             if (massiv[i] % 2 != 0) {
                 massiv[i]++;
